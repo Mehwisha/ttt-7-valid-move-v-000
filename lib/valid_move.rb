f71afb16dc6c3c6d46ebe4a7592142cf70b19fd4
Mehwish Aziz <mehwisha@msn.com>
2018-04-13 02:20:48 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move? (board, index)
-  if board[index] == 8.between?(0,8) ||  board[index] == " "
+  if board[index] == 8.between?(0,8) &&  board[index] == " "
   true
 else board[index] != " " &&  board[index] == nil
   false
