596e32b310f1eec1f169d52712db9780ab51144c
Mehwish Aziz <mehwisha@msn.com>
2018-04-13 02:06:55 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,7 @@
 # code your #valid_move? method here
 def valid_move? (board, index)
-  if  board[index] == 8.between?(0,8)
-    true
-elsif position_taken?
-  false
+  if position_taken? == 8.between?(0,8)
+  true : false
 end
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
