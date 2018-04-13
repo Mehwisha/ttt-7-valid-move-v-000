05003a6c0ba52f5992a56508e28eacb794a06e60
Mehwish Aziz <mehwisha@msn.com>
2018-04-13 02:02:09 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ elsif position_taken?
 end
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken(board, index)
+def position_taken?(board, index)
 if board[index] == " " || board[index] == ""
   true
 elsif board[index] == "X" || board[index] == "Y"
